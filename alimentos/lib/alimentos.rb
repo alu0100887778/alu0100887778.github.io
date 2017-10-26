abfd73bbda674de8fc812ec22baa1875cfe8e8f7
criscrack <cristian_gonzalez_alonso@hotmail.com>
2017-10-26 15:32:42 +0000

    Estructura de la gema creada y clase alimentos con variables definida

---
+++ alimentos/lib/alimentos.rb
@@ -0,0 +1,22 @@
+require "alimentos/version.rb"
+
+module Alimentos
+
+class Alimentos
+    
+attr_reader :alimentos, :proteinas, :glucidos, :lipidos
+
+#Inicializamos las variables
+
+def initialize 
+    
+    @alimento = alimento
+    @proteinas = proteinas
+    @glucidos = glucidos
+    @lipidos = lipidos
+    
+end
+    
+    
+end
+end
